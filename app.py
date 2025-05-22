8a6faa26f695e08694d6af4f364e95f00933dd3e
Jeff Hall <rhythmatician5@gmail.com>
2025-05-22 18:48:13 -0500

    fix: update app.run configuration for production settings

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ def validate_token(token):
 
 if __name__ == "__main__":
     get_openid_config()
-    app.run(debug=True)
+    app.run(host="0.0.0.0",
+            port=8080,
+            debug=False)
