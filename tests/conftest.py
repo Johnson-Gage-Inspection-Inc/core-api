c262d9e0c03aef4106d7c4b083deea6265c3862c
Jeff Hall <rhythmatician5@gmail.com>
2025-05-21 19:52:24 -0500

    Rename main blueprint to work_item_details_bp and update imports in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,16 @@
 from app import app as flask_app
 from flask import Flask
-from routes.work_item_details import bp as main_bp
+from routes.work_item_details import bp as work_item_details_bp
+from routes.whoami import bp as whoami_bp
 import pytest
 
 @pytest.fixture
 def client():
     app = Flask(__name__)
-    app.register_blueprint(main_bp)
     app.config["TESTING"] = True
+
+    app.register_blueprint(work_item_details_bp)
+    app.register_blueprint(whoami_bp)
+
     with app.test_client() as client:
         yield client
