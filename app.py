9d12b6cadd3a965c480d84ad1c0202674e479c3d
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-05-22 19:31:57 +0000

    /docs endpoint isn't properly constructed by Swagger Fixes #3

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/docs"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+app.config["OPENAPI_SECURITY_SCHEMES"] = {
+    "BearerAuth": {
+        "type": "http",
+        "scheme": "bearer",
+        "bearerFormat": "JWT"
+    }
+}
 
 api = Api(app)
 
